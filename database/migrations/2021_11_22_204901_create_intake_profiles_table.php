f655f434ffad5712fa39df6e127a84eccc31ec17
richard wakeham <rwakeham27@outlook.com>
2021-11-22 21:09:10 +0000

    Created IntakeProfile model and table

---
+++ database/migrations/2021_11_22_204901_create_intake_profiles_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateIntakeProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('intake_profiles', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->unsignedInteger('max_calories');
+            $table->unsignedDouble('max_total_fat');
+            $table->unsignedDouble('max_saturated_fat');
+            $table->unsignedDouble('max_total_sugar');
+            $table->unsignedDouble('max_salt');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('intake_profiles');
+    }
+}
