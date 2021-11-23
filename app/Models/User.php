bac336b3a3b16d0ac8c294407a2becbea822b22e
richard wakeham <rwakeham27@outlook.com>
2021-11-23 10:58:40 +0000

    Added one-to-one relationship between intake profiles and users

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,11 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * Gets the user's intake profile.
+     */
+    public function intakeProfile() {
+        return $this->belongsTo(IntakeProfile::class);
+    }
 }
