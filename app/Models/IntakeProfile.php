ae05b2519ded6f133c0c18c5af94caa5a197cf29
richard wakeham <rwakeham27@outlook.com>
2021-11-23 10:25:56 +0000

    Added one-to-one relationship between intake profiles and users

--- app/Models/IntakeProfile.php
+++ app/Models/IntakeProfile.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class IntakeProfile extends Model
 {
     use HasFactory;
+
+    /**
+     * Gets the user who has this intake profile.
+     */
+    public function user() {
+        return $this->hasOne(User::class);
+    }
 }
