a99f30e6c2ea32d0a29684494a5ab90eb18f62b4
richard wakeham <rwakeham27@outlook.com>
2021-12-29 18:59:52 +0000

    Add age category enum

---
+++ app/Enums/AgeCategory.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Enums;
+
+enum AgeCategory: String
+{
+    case FOUR_TO_SIX = '4-6';
+    case SEVEN_TO_TEN = '7-10';
+    case ELEVEN_TO_FOURTEEN = '11-14';
+    case FIFTEEN_TO_EIGHTEEN = '15-18';
+    case NINETEEN_TO_SIXTY_FOUR = '19-64';
+    case SIXTY_FIVE_TO_SEVENTY_FOUR = '65-74';
+    case SEVENTY_FOUR_PLUS = '74+';
+}
+
+?>
