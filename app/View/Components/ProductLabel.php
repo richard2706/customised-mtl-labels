dabe3771262fc0af4336ecc48a11f638858fdf7c
richard wakeham <rwakeham27@outlook.com>
2022-01-16 15:21:30 +0000

    Display nutrition values per portion or 100g/ml

--- app/View/Components/ProductLabel.php
+++ app/View/Components/ProductLabel.php
@@ -7,8 +7,29 @@ use OpenFoodFacts;
 
 class ProductLabel extends Component
 {
+    /** Whether there is sufficient data to generate the label. */
+    public $labelSuccessful;
+
     /** Name of the product. */
     public $productName;
+
+    /** Size of each portion of the product. */
+    public $portionSize;
+
+    /** Nutrition values per portion or 100g/ml */
+    public $nutrientValues;
+
+    /** Energy values per 100 g/ml */
+    public $energyKJPer100;
+    public $energyKcalPer100;
+
+    /** Units for each nutrition value. */
+    public $energyKJUnits;
+    public $energyKcalUnits;
+    public $productUnits;
+
+    /** Default value for unknown values. */
+    private const DEFAULT_VALUE = 'unknown';
 
     /**
      * Create a new product label component instance. Note that the barcode is assumed to be valid,
@@ -19,7 +40,38 @@ class ProductLabel extends Component
     public function __construct($barcode)
     {
         $product = OpenFoodFacts::barcode($barcode);
-        $this->productName = $product['product_name'];
+        $this->productName = array_key_exists('product_name', $product) ? $product['product_name'] : self::DEFAULT_VALUE;
+        $this->productUnits = 'g'; // work out if g or ml
+
+        // Find nutrition values per 100g/ml
+        $per100Keys = ['energy-kj_100g', 'energy-kcal_100g', 'fat_100g', 'saturated-fat_100g', 'sugars_100g', 'salt_100g'];
+        $per100Exists = $this->count_array_keys($per100Keys, $product['nutriments']) >= 4;
+        if ($per100Exists) {
+            // Find nutrition values per 100g/ml
+            $labelKeys = ['energy-kj', 'energy-kcal', 'fat', 'saturated-fat', 'sugars', 'salt'];
+            foreach ($labelKeys as $key) {
+                $this->nutrientValues[$key] = array_key_exists($key, $product['nutriments']) ? floatval($product['nutriments'][$key]) : null;
+            }
+            $this->labelSuccessful = true;
+            
+            // Adjust label values for specified number of portions
+            $numPortions = 1;
+            $this->portionSize = array_key_exists('serving_size', $product) ? floatval($product['serving_size']) : 100;
+            foreach ($this->nutrientValues as $key => $value) {
+                if (!is_null($value)) {
+                    $this->nutrientValues[$key] *= $numPortions * $this->portionSize / 100;
+                }
+            }
+
+            // Get energy values per 100g/ml
+            $this->energyKJPer100 = array_key_exists('energy-kj_100g', $product['nutriments']) ? $product['nutriments']['energy-kj_100g'] : null;
+            $this->energyKcalPer100 = array_key_exists('energy-kcal_100g', $product['nutriments']) ? $product['nutriments']['energy-kcal_100g'] : null;
+        } else {
+            $this->labelSuccessful = false;
+        }
+
+        $this->energyKJUnits = array_key_exists('energy-kj_unit', $product['nutriments']) ? $product['nutriments']['energy-kj_unit'] : 'kJ';
+        $this->energyKcalUnits = array_key_exists('energy-kcal_unit', $product['nutriments']) ? $product['nutriments']['energy-kcal_unit'] : 'kcal';
     }
 
     /**
@@ -30,5 +82,12 @@ class ProductLabel extends Component
     public function render()
     {
         return view('components.product-label');
+    }
+
+    /**
+     * Returns the number of keys present in the given array. e.g. ['a'], ['a' => 1, 'b' => 2] returns 1.
+     */
+    private function count_array_keys(array $keys, array $array) {
+        return count(array_intersect_key(array_flip($keys), $array));
     }
 }
--- resources/views/components/product-label.blade.php
+++ resources/views/components/product-label.blade.php
@@ -1,4 +1,32 @@
 <div>
-    <p>product info</p>
     <p>{{ $productName }}</p>
+    <p>{{ $portionSize }} {{ $productUnits }} contains:</p>
+    <div>
+        <div>
+            <p>Energy</p>
+            <p>{{ $nutrientValues['energy-kj'] }} {{ $energyKJUnits }}</p>
+            <p>{{ $nutrientValues['energy-kcal'] }} {{ $energyKcalUnits }}</p>
+            <p></p>
+        </div>
+        <div>
+            <p>Fat</p>
+            <p>{{ $nutrientValues['fat'] }} {{ $productUnits }}</p>
+        </div>
+        <div>
+            <p>Saturates</p>
+            <p>{{ $nutrientValues['saturated-fat'] }} {{ $productUnits }}</p>
+        </div>
+        <div>
+            <p>Sugars</p>
+            <p>{{ $nutrientValues['sugars'] }} {{ $productUnits }}</p>
+        </div>
+        <div>
+            <p>Salt</p>
+            <p>{{ $nutrientValues['salt'] }} {{ $productUnits }}</p>
+        </div>
+    </div>
+    <div>
+        <p>of an adult's reference intake</p>
+        <p>Energy per 100 {{ $productUnits }}: {{ $energyKJPer100 }} {{ $energyKJUnits }} / {{ $energyKcalPer100 }} {{ $energyKcalUnits }}</p>
+    </div>
 </div>
