3e9b216da07212e6ff8a993c77d860f5ea4413a7
richard wakeham <rwakeham27@outlook.com>
2022-01-17 11:25:30 +0000

    Format product label

--- resources/views/components/product-label.blade.php
+++ resources/views/components/product-label.blade.php
@@ -1,84 +1,97 @@
-<div>
-    <p>{{ $productName }}</p>
+<div class="inline-block text-center">
+    <b class="text-lg">{{ $productName }}</b>
     <p>{{ $portionSize }} {{ $productUnits }} contains:</p>
-    <div>
-        <div>
-            <p>Energy</p>
-            @if (!is_null($nutrientValues['energy-kj']))
-                <p>{{ round($nutrientValues['energy-kj']) }} {{ $energyKJUnits }}</p>
-            @endif
-            @if (!is_null($nutrientValues['energy-kcal']))
-                <p>{{ round($nutrientValues['energy-kcal']) }} {{ $energyKcalUnits }}</p>
-            @else
-                <p>Unknown calories</p>
-            @endif
-            @if ($percentageIntakes['energy-kcal'] < 1)
-                <p>&lt;1%</p>
-            @else
-                <p>{{ round($percentageIntakes['energy-kcal']) }}%</p>
-            @endif
+
+    <div class="inline-flex space-x-0.5">
+        <div class="border border-black rounded p-1 text-center flex flex-col">
+            <p class="text-sm">Energy</p>
+            <div class="flex flex-col">
+                @if (!is_null($nutrientValues['energy-kj']))
+                    <p>{{ round($nutrientValues['energy-kj']) }} {{ $energyKJUnits }}</p>
+                @endif
+                @if (!is_null($nutrientValues['energy-kcal']))
+                    <p>{{ round($nutrientValues['energy-kcal']) }} {{ $energyKcalUnits }}</p>
+                @else
+                    <p class="text-xs">Unknown calories</p>
+                @endif
+            </div>
+            <p class="mt-auto">
+                @if ($percentageIntakes['energy-kcal'] < 1)
+                    &lt;1%
+                @else
+                    {{ round($percentageIntakes['energy-kcal']) }}%
+                @endif
+            </p>
         </div>
 
-        <div>
-            <p>Fat</p>
+        <div class="border border-black rounded p-1 text-center flex flex-col">
+            <p class="text-sm">Fat</p>
             @if (!is_null($nutrientValues['fat']))
                 <p>{{ round($nutrientValues['fat'], 1) }} g</p>
-                @if ($percentageIntakes['fat'] < 1)
-                    <p>&lt;1%</p>
-                @else
-                    <p>{{ round($percentageIntakes['fat']) }}%</p>
-                @endif
+                <p class="mt-auto">
+                    @if ($percentageIntakes['fat'] < 1)
+                        &lt;1%
+                    @else
+                        {{ round($percentageIntakes['fat']) }}%
+                    @endif
+                </p>
             @else
-                <p>Unknown</p>
+                <p class="text-xs">Unknown</p>
             @endif
         </div>
 
-        <div>
-            <p>Saturates</p>
+        <div class="border border-black rounded p-1 text-center flex flex-col">
+            <p class="text-sm">Saturates</p>
             @if (!is_null($nutrientValues['saturated-fat']))
                 <p>{{ round($nutrientValues['saturated-fat'], 1) }} g</p>
-                @if ($percentageIntakes['saturated-fat'] < 1)
-                    <p>&lt;1%</p>
-                @else
-                    <p>{{ round($percentageIntakes['saturated-fat']) }}%</p>
-                @endif
+                <p class="mt-auto">
+                    @if ($percentageIntakes['saturated-fat'] < 1)
+                        &lt;1%
+                    @else
+                        {{ round($percentageIntakes['saturated-fat']) }}%
+                    @endif
+                </p>
             @else
-                <p>Unknown</p>
+                <p class="text-xs">Unknown</p>
             @endif
         </div>
 
-        <div>
-            <p>Sugars</p>
+        <div class="border border-black rounded p-1 text-center flex flex-col">
+            <p class="text-sm">Sugars</p>
             @if (!is_null($nutrientValues['sugars']))
                 <p>{{ round($nutrientValues['sugars'], 1) }} g</p>
-                @if ($percentageIntakes['sugars'] < 1)
-                    <p>&lt;1%</p>
-                @else
-                    <p>{{ round($percentageIntakes['sugars']) }}%</p>
-                @endif
+                <p class="mt-auto">
+                    @if ($percentageIntakes['sugars'] < 1)
+                        &lt;1%
+                    @else
+                        {{ round($percentageIntakes['sugars']) }}%
+                    @endif
+                </p>
             @else
-                <p>Unknown</p>
+                <p class="text-xs">Unknown</p>
             @endif
         </div>
 
-        <div>
-            <p>Salt</p>
+        <div class="border border-black rounded p-1 text-center flex flex-col">
+            <p class="text-sm">Salt</p>
             @if (!is_null($nutrientValues['salt']))
                 <p>{{ round($nutrientValues['salt'], 2) }} g</p>
-                @if ($percentageIntakes['salt'] < 1)
-                    <p>&lt;1%</p>
-                @else
-                    <p>{{ round($percentageIntakes['salt']) }}%</p>
-                @endif
+                <p class="mt-auto">
+                    @if ($percentageIntakes['salt'] < 1)
+                        &lt;1%
+                    @else
+                        {{ round($percentageIntakes['salt']) }}%
+                    @endif
+                </p>
             @else
-                <p>Unknown</p>
+                <p class="text-xs">Unknown</p>
             @endif
         </div>
     </div>
 
     <div>
-        <p>of an adult's reference intake</p>
-        <p>Energy per 100{{ $productUnits }}:
+        <p>of your reference intake</p>
+        <p class="text-sm">Energy per 100{{ $productUnits }}:
             @if (!is_null($energyKJPer100))
                 {{ $energyKJPer100 . ' ' . $energyKJUnits}}
             @endif
