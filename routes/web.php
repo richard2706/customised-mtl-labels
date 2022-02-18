3ff0c1525d200e239517d84cbed060973f261fce
richard wakeham <rwakeham27@outlook.com>
2022-02-18 19:20:34 +0000

    Remove test code

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-use App\Enums\AgeCategory;
-use App\Enums\Gender;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
@@ -39,10 +37,5 @@ Route::post('/scan', [ProductController::class, 'findProduct'])
 
 Route::get('/product/{barcode}', [ProductController::class, 'label'])
     ->middleware(['auth'])->name('product.show');
-
-Route::get('/test', function() {
-    $profile = AgeCategory::FOUR_TO_SIX->maxIntakeProfile(Gender::FEMALE);
-    dd($profile);
-});
 
 require __DIR__.'/auth.php';
