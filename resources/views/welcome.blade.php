eda4254ba8b60b107d4f9ecebd612df2a842657e
Richard Wakeham <rwakeham27@outlook.com>
2022-03-28 12:25:35 +0100

    Add create account button on welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,8 @@
     </div>
 
     <x-slot name="footer">
-        <div class="flex flex-col items-center p-2">
+        <div class="flex flex-col items-center space-y-2 p-2">
+            <x-button-secondary href="{{ route('register') }}">Create an Account</x-button-secondary>
             <x-button-primary href="{{ route('login') }}">Login</x-button-primary>
         </div>
     </x-slot>
