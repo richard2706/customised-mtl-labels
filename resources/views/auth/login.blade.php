0bf93df66321d65e6a7a5063de235736477fdb38
Richard Wakeham <rwakeham27@outlook.com>
2022-03-28 13:51:59 +0100

    Adjust styling for login form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,5 +1,7 @@
 <x-guest-layout>
-    <div class="pb-28">
+    <x-slot name="header">Please Log In</x-slot>
+
+    <div class="mt-2 pb-28">
         <!-- Session Status -->
         <x-auth-session-status class="mb-4" :status="session('status')" />
 
@@ -9,44 +11,46 @@
         <form method="POST" action="{{ route('login') }}" id="login-form">
             @csrf
 
-            <!-- Email Address -->
-            <div>
-                <x-label for="email" :value="__('Email')" />
-
-                <x-input id="email" class="block mt-1" type="email" name="email" :value="old('email')" required autofocus />
-            </div>
-
-            <!-- Password -->
-            <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
-
-                <x-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="current-password" />
-            </div>
-
-            <!-- Remember Me -->
-            <div class="block mt-4">
-                <label for="remember_me" class="inline-flex items-center">
-                    <input id="remember_me" type="checkbox" class="text-nutrient-med focus:border-nutrient-med focus:ring focus:ring-nutrient-med focus:ring-opacity-50" name="remember">
-                    <span class="ml-2 text-sm">{{ __('Remember me') }}</span>
-                </label>
-            </div>
-
-            <x-slot name="footer">
-                <div class="flex flex-col items-center space-y-2 p-2">
-                    @if (Route::has('password.request'))
-                        <x-button-secondary href="{{ route('password.request') }}">
-                            {{ __('Forgot your password?') }}
-                        </x-button-secondary>
-                    @endif
-        
-                    <x-button-primary class="cursor-pointer" onclick="document.getElementById('login-form').submit();">
-                        {{ __('Log in') }}
-                    </x-button-primary>
+            <div class="flex flex-col items-center">
+                <!-- Email Address -->
+                <div class="w-full max-w-sm">
+                    <x-label for="email" :value="__('Email')" />
+    
+                    <x-input id="email" class="block mt-1" type="email" name="email" :value="old('email')" required autofocus />
                 </div>
-            </x-slot>
+    
+                <!-- Password -->
+                <div class="mt-4 w-full max-w-sm">
+                    <x-label for="password" :value="__('Password')" />
+    
+                    <x-input id="password" class="block mt-1 w-full"
+                                    type="password"
+                                    name="password"
+                                    required autocomplete="current-password" />
+                </div>
+    
+                <!-- Remember Me -->
+                <div class="block mt-4">
+                    <label for="remember_me" class="inline-flex items-center">
+                        <input id="remember_me" type="checkbox" class="text-nutrient-med focus:border-nutrient-med focus:ring focus:ring-nutrient-med focus:ring-opacity-50" name="remember">
+                        <span class="ml-2 text-sm">{{ __('Remember me') }}</span>
+                    </label>
+                </div>
+    
+                <x-slot name="footer">
+                    <div class="flex flex-col items-center space-y-2 p-2">
+                        @if (Route::has('password.request'))
+                            <x-button-secondary href="{{ route('password.request') }}">
+                                {{ __('Forgot your password?') }}
+                            </x-button-secondary>
+                        @endif
+            
+                        <x-button-primary class="cursor-pointer" onclick="document.getElementById('login-form').submit();">
+                            {{ __('Log in') }}
+                        </x-button-primary>
+                    </div>
+                </x-slot>
+            </div>
         </form>
     </div>
 </x-guest-layout>
--- resources/views/components/button-secondary.blade.php
+++ resources/views/components/button-secondary.blade.php
@@ -1,4 +1,4 @@
-<a {{ $attributes->merge(['class' => 'w-full max-w-sm box-border border-2 border-nutrient-med text-center']) }}>
+<a {{ $attributes->merge(['class' => 'w-full max-w-sm box-border border-2 border-nutrient-high text-center']) }}>
     <div class="py-2 px-2 sm:hover:bg-black sm:hover:bg-opacity-10">
         {{ $slot }}
     </div>
