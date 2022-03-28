13d15bb2efb2bcde1459f2806750c36247c4543b
Richard Wakeham <rwakeham27@outlook.com>
2022-03-28 19:05:44 +0100

    Add vue.js successfully

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,11 @@
 require('./bootstrap');
 
-import Alpine from 'alpinejs';
+import { createApp } from 'vue'
 
-window.Alpine = Alpine;
-
-Alpine.start();
+createApp({
+    data() {
+        return {
+            'message': 'test1'
+        }
+    }
+}).mount('#app');
--- resources/views/product/scan.blade.php
+++ resources/views/product/scan.blade.php
@@ -5,7 +5,7 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
+    <div class="py-12" id="app">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
@@ -14,6 +14,9 @@
                             <p>{{ $error }}</p>
                         @endforeach
                     @endif
+
+                    <p>barcode reader</p>
+                    <p>@{{ message }}</p>
 
                     <form method="POST" action="{{ route('product.find') }}">
                         @csrf
