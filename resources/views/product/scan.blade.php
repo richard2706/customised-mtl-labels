d92c0d2a734742fb9a17afbd685027d23078dd38
Richard Wakeham <rwakeham27@outlook.com>
2022-03-30 12:11:48 +0100

    Move manual barcode form

--- resources/views/product/scan.blade.php
+++ resources/views/product/scan.blade.php
@@ -3,21 +3,23 @@
         Scan a Product
     </x-slot>
 
-    <div class="border-2 border-black h-80 flex items-center justify-center">
-        <p>Camera here</p>
+    <div class="flex flex-col items-center">
+        <div class="border-2 border-black h-80 w-full flex items-center justify-center">
+            <p>Camera here</p>
+        </div>
+
+        <form method="POST" action="{{ route('product.find') }}" id="barcode-num-form" class="w-full max-w-sm mt-4">
+            @csrf
+            <div class="flex flex-row justify-center space-x-2">
+                <x-input type="text" name="barcode" value="" placeholder="Manually Enter Barcode" required/>
+                <x-button-submit form="barcode-num-form" class="max-w-max">Done</x-button>
+            </div>
+        </form>
     </div>
 
     <x-slot name="footer">
         <div class="flex flex-col items-center space-y-2 p-2">
             <x-button-secondary href="{{ route('dashboard') }}">Back to Home</x-button-secondary>
-            
-            <form method="POST" action="{{ route('product.find') }}" id="barcode-num-form" class="w-full max-w-lg">
-                @csrf
-                <div class="flex flex-row space-x-2">
-                    <x-input type="text" name="barcode" value="" placeholder="Barcode number" required/>
-                    <x-button-submit form="barcode-num-form" class="max-w-max">Show product label</x-button>
-                </div>
-            </form>
         </div>
     </x-slot>
 </x-app-layout>
