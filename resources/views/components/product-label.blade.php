c876107022229e87c51d71762b35320ac7a7fd48
Richard Wakeham <rwakeham27@outlook.com>
2022-03-30 10:44:11 +0100

    Adjust padding for product labels

--- resources/views/components/product-label.blade.php
+++ resources/views/components/product-label.blade.php
@@ -7,7 +7,7 @@
 
         <div class="inline-flex space-x-0.5">
 
-            <div class="border border-black rounded p-1 text-center flex flex-col">
+            <div class="border border-black rounded p-1.5 text-center flex flex-col">
                 <p class="text-sm">Energy</p>
                 <div class="flex flex-col">
                     @if (!is_null($nutrientValues['energy-kj']))
@@ -28,7 +28,7 @@
                 </p>
             </div>
 
-            <div class="border border-black rounded p-1 text-center flex flex-col {{ $nutrientColourStyles['fat'] }}">
+            <div class="border border-black rounded p-1.5 text-center flex flex-col {{ $nutrientColourStyles['fat'] }}">
                 <p class="text-sm">Fat</p>
                 @if (!is_null($nutrientValues['fat']))
                     <p>{{ round($nutrientValues['fat'], 1) }} g</p>
@@ -44,7 +44,7 @@
                 @endif
             </div>
 
-            <div class="border border-black rounded p-1 text-center flex flex-col {{ $nutrientColourStyles['saturated-fat'] }}">
+            <div class="border border-black rounded p-1.5 text-center flex flex-col {{ $nutrientColourStyles['saturated-fat'] }}">
                 <p class="text-sm">Saturates</p>
                 @if (!is_null($nutrientValues['saturated-fat']))
                     <p>{{ round($nutrientValues['saturated-fat'], 1) }} g</p>
@@ -60,7 +60,7 @@
                 @endif
             </div>
 
-            <div class="border border-black rounded p-1 text-center flex flex-col {{ $nutrientColourStyles['sugars'] }}">
+            <div class="border border-black rounded p-1.5 text-center flex flex-col {{ $nutrientColourStyles['sugars'] }}">
                 <p class="text-sm">Sugars</p>
                 @if (!is_null($nutrientValues['sugars']))
                     <p>{{ round($nutrientValues['sugars'], 1) }} g</p>
@@ -76,7 +76,7 @@
                 @endif
             </div>
 
-            <div class="border border-black rounded p-1 text-center flex flex-col {{ $nutrientColourStyles['salt'] }}">
+            <div class="border border-black rounded p-1.5 text-center flex flex-col {{ $nutrientColourStyles['salt'] }}">
                 <p class="text-sm">Salt</p>
                 @if (!is_null($nutrientValues['salt']))
                     <p>{{ round($nutrientValues['salt'], 2) }} g</p>
