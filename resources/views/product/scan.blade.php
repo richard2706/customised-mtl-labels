8687783dcd59a2206e48f0cf60f6493b43f7f38d
Richard Wakeham <rwakeham27@outlook.com>
2022-03-30 10:15:09 +0100

    Style barcode scan page

--- resources/views/product/scan.blade.php
+++ resources/views/product/scan.blade.php
@@ -3,27 +3,21 @@
         Scan a Product
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    @if ($errors->any)
-                        @foreach ($errors->all() as $error)
-                            <p>{{ $error }}</p>
-                        @endforeach
-                    @endif
-
-                    <form method="POST" action="{{ route('product.find') }}">
-                        @csrf
-
-                        <x-label for="name">Barcode number</x-label>
-                        <x-input type="text" name="barcode" value=""/>
-                        <br>
-
-                        <x-button class="mt-2">Show product label</x-button>
-                    </form>
-                </div>
-            </div>
-        </div>
+    <div class="border-2 border-black h-80 flex items-center justify-center">
+        <p>Camera here</p>
     </div>
+
+    <x-slot name="footer">
+        <div class="flex flex-col items-center space-y-2 p-2">
+            <x-button-secondary href="{{ route('dashboard') }}">Back to Home</x-button-secondary>
+            
+            <form method="POST" action="{{ route('product.find') }}" id="barcode-num-form" class="w-full max-w-lg">
+                @csrf
+                <div class="flex flex-row space-x-2">
+                    <x-input type="text" name="barcode" value="" placeholder="Barcode number" required/>
+                    <x-button-submit form="barcode-num-form" class="max-w-max">Show product label</x-button>
+                </div>
+            </form>
+        </div>
+    </x-slot>
 </x-app-layout>
