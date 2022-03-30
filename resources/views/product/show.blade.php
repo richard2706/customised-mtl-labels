18b03513d5d0b5bfc412064198b0228a402ebb80
Richard Wakeham <rwakeham27@outlook.com>
2022-03-30 10:24:21 +0100

    Update show.blade.php

--- resources/views/product/show.blade.php
+++ resources/views/product/show.blade.php
@@ -15,7 +15,7 @@
     <x-slot name="footer">
         <div class="flex flex-col items-center space-y-2 p-2">
             <x-button-secondary href="{{ route('dashboard') }}">Back to Home</x-button-secondary>
-            <x-button-secondary href="{{ route('product.scan') }}">Scan Another Barcode</x-button-secondary>
+            <x-button-primary href="{{ route('product.scan') }}">Scan Another Barcode</x-button-primary>
         </div>
     </x-slot>
 </x-app-layout>
