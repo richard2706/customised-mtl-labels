08d8ecbe8a86ef5f178df7ca818c153be5dd1b82
Richard Wakeham <rwakeham27@outlook.com>
2022-04-01 15:31:07 +0100

    Add app description to welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,8 @@
 <x-guest-layout>
-    <div class="flex flex-col items-center mt-2 short:pb-32">
-        <p>Welcome page</p>
-        <p>TODO write a welcome message and show demo/screenshots</p>
+    <div class="flex flex-col text-center items-center gap-y-4 mx-auto max-w-xl short:pb-32">
+        <p>Scan any product's barcode then you will be shown its traffic light nutrition label. This provides a visual representation of the nutritional value of one portion of the product.</p>
+        <p>If you log in, then you can customise for each nutrient, when the traffic light label shows it as green, amber or red.</p>
+        <p>You can also customise your daily maximum for each nutrient, then the percentages will reflect the amount each nutrient (in one portion of the product) contributes to your daily maximum.</p>
     </div>
 
     <x-slot name="footer">
