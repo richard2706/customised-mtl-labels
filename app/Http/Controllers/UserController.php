f8e1127dd5104d24b62a0e892017fe36111922ec
Richard Wakeham <rwakeham27@outlook.com>
2022-04-03 11:37:44 +0100

    Show scan history for current user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -18,7 +18,7 @@ class UserController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function showDashboard() {
-        $scanHistoryEntries = ScanHistory::orderBy('created_at', 'desc')->get();
+        $scanHistoryEntries = ScanHistory::where('user_id', '=', Auth::user()->id)->orderBy('created_at', 'desc')->get();
 
         // Split created time and date into atomic values
         foreach ($scanHistoryEntries as $entry => $data) {
